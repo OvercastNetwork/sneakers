863f00f6bed3267817994255ab5f15f259ee0947
Jedediah Smith <jedediah@silencegreys.com>
2014-12-17 23:04:54 -0500

    Don't ignore Runner options

--- lib/sneakers/runner.rb
+++ lib/sneakers/runner.rb
@@ -4,7 +4,7 @@ require 'sneakers/workergroup'
 module Sneakers
   class Runner
     def initialize(worker_classes, opts={})
-      @runnerconfig = RunnerConfig.new(worker_classes)
+      @runnerconfig = RunnerConfig.new(worker_classes, opts)
     end
 
     def run
@@ -29,9 +29,9 @@ module Sneakers
       end
     end
 
-    def initialize(worker_classes)
+    def initialize(worker_classes, opts={})
       @worker_classes = worker_classes
-      @conf = {}
+      @conf = opts
     end
 
     def to_h
@@ -40,7 +40,6 @@ module Sneakers
 
 
     def reload_config!
-      Sneakers.logger.warn("Loading runner configuration...")
       config_file = Sneakers::CONFIG[:runner_config_file]
 
       if config_file
